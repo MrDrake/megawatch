51b85007ae726fdc9712afd96c9efc12f940272a
MrDrake <john@mrdrake.net>
2016-01-12 16:03:52 +1100

    Basalt version of MegaWatch

--- src/main.c
+++ src/main.c
@@ -1,60 +1,106 @@
 #include <pebble.h>
 
 static Window *s_main_window;
-static Layer *s_time_layer;
+static TextLayer *s_time_layer;
 
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
-static void update_time(Layer *layer, GContext *ctx) {
-  time_t temp = time(NULL); 
-  struct tm *t = localtime(&temp);
+static GFont s_time_font;
 
-  GRect bounds = GRect(12, 12, 156, 156);
-  graphics_context_set_stroke_width(ctx, 2);
-  graphics_draw_circle(ctx, gpoint_from_polar(bounds, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(t->tm_hour*30)), 6);
-  graphics_draw_circle(ctx, gpoint_from_polar(bounds, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(t->tm_min*6)), 3);
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  layer_mark_dirty(window_get_root_layer(s_main_window));
+  update_time();
 }
 
 static void main_window_load(Window *window) {
+  // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_MEGAWATCH_BACKGROUND);
+  // Create GBitmap
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+
+  // Create BitmapLayer to display the GBitmap
   s_background_layer = bitmap_layer_create(bounds);
 
+  // Set the bitmap onto the layer and add to the window
   bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));  
-    
-  s_time_layer = layer_create(bounds);
-  layer_set_update_proc(s_time_layer, update_time);
-  layer_add_child(window_layer, s_time_layer);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
+  // Create the TextLayer with specific bounds
+  s_time_layer = text_layer_create(
+      GRect(0, 52, bounds.size.w, 50));
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+
+  // Apply to TextLayer
+  text_layer_set_font(s_time_layer, s_time_font);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
-  layer_destroy(s_time_layer);
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
+
+  // Unload GFont
+  fonts_unload_custom_font(s_time_font);
+
+  // Destroy GBitmap
   gbitmap_destroy(s_background_bitmap);
+
+  // Destroy BitmapLayer
   bitmap_layer_destroy(s_background_layer);
 }
 
 
 static void init() {
+  // Create main Window element and assign to pointer
   s_main_window = window_create();
+
+  // Set the background color
+  window_set_background_color(s_main_window, GColorWhite);
+
+  // Set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload
   });
-  window_set_background_color(s_main_window, GColorWhite);  
+
+  // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
-    
+
+  // Make sure the time is displayed from the start
+  update_time();
+
+  // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit() {
+  // Destroy Window
   window_destroy(s_main_window);
 }
 
